29a27eaee2e8f09bab829ed3bf30225ea31d1a01
LuisBelo <luzaidinbelo@gmail.com>
2020-10-11 12:38:15 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import L from 'leaflet';
+
+fetch('./data/Regioes_Brasil.geojson')
+	.then((file) => file.json())
+	.then((brazil_regions) => {
+		console.log('brazil_regions', brazil_regions);
+		const bounds = [
+			[-40, -80],
+			[13, -20],
+		];
+		const map = L.map('map', {
+			center: [-15, -50],
+			zoom: 5,
+			maxZoom: 6,
+			minZoom: 5,
+			zoomControl: false,
+			maxBounds: bounds,
+		});
+
+		const brazil_regions_layer = L.geoJSON(brazil_regions, { color: '#fff' }).addTo(map);
+	});
