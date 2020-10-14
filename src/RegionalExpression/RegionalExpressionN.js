a52d52e0a1ee44fb1ade6ed737ef0ca3e5c1035e
LuisBelo <luzaidinbelo@gmail.com>
2020-10-13 22:18:20 -0300

    refactor: add  expressions

--- src/RegionalExpression/RegionalExpressionN.js
+++ src/RegionalExpression/RegionalExpressionN.js
@@ -1,9 +1,87 @@
 const regionalExpressionsN = [
     {
-        id: "",
-        expressao: "",
-        regiao: "Norte",
-        descricao: ""
+        id: "N1",
+        expression: "Brocado",
+        region: "Norte",
+        meaning: "Estar com fome"
+    },
+    {
+        id: "N2",
+        expression: "Capar o gato",
+        region: "Norte",
+        meaning: "Ir embora"
+    },
+    {
+        id: "N3",
+        expression: "Égua de largura",
+        region: "Norte",
+        meaning: "Muita sorte"
+    },
+    {
+        id: "N4",
+        expression: "Levou o farelo",
+        region: "Norte",
+        meaning: "Morreu"
+    },
+    {
+        id: "N5",
+        expression: "Miudinho",
+        region: "Norte",
+        meaning: "Pequeno"
+    },
+    {
+        id: "N6",
+        expression: "Teú",
+        region: "Norte",
+        meaning: "Lagarto"
+    },
+    {
+        id: "N7",
+        expression: "Vigia bem",
+        region: "Norte",
+        meaning: "Presta muita atenção"
+    },
+    {
+        id: "N8",
+        expression: "Gaiato",
+        region: "Norte",
+        meaning: "Pessoa cheia de gracinhas"
+    },
+    {
+        id: "N9",
+        expression: "Paga uma aí",
+        region: "Norte",
+        meaning: "Pagar uma bebida"
+    },
+    {
+        id: "N10",
+        expression: "Popudinho",
+        region: "Norte",
+        meaning: "Pessoa alcóolatra"
+    },
+    {
+        id: "N11",
+        expression: "Umborimbora?",
+        region: "Norte",
+        meaning: "Vamos embora?"
+    },
+    {
+        id: "N12",
+        expression: "Zé ruela",
+        region: "Norte",
+        meaning: "Abestado, besta"
+    },
+    {
+        id: "N13",
+        expression: "De rocha",
+        region: "Norte",
+        meaning: "Palavra ou assunto com convicção"
+    },
+    {
+        id: "N14",
+        expression: "Essa é da grife do varal",
+        region: "Norte",
+        meaning: "Roupa roubada"
     },
 ];
 
