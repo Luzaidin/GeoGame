dbf99efecf1f7e1c00d6f6bc0e45005dc8e1ec22
LuisBelo <luzaidinbelo@gmail.com>
2020-10-14 23:00:57 -0300

    refactor: add SE expressions

---
+++ src/RegionalExpression/RegionalExpressionSE.js
@@ -0,0 +1,154 @@
+const regionalExpressionsSE = [
+    {
+        id: "SE1",
+        expression: "Bolado",
+        region: "Sudeste",
+        meaning: "Preocupado"
+    },
+    {
+        id: "SE2",
+        expression: "Da hora",
+        region: "Sudeste",
+        meaning: "Legal"
+    },
+    {
+        id: "SE3",
+        expression: "É fria",
+        region: "Sudeste",
+        meaning: "É perigoso"
+    },
+    {
+        id: "SE4",
+        expression: "Jacú",
+        region: "Sudeste",
+        meaning: "Bobo, ignorante"
+    },
+    {
+        id: "SE5",
+        expression: "Larica",
+        region: "Sudeste",
+        meaning: "Fome"
+    },
+    {
+        id: "SE6",
+        expression: "Quebrado",
+        region: "Sudeste",
+        meaning: "Sem dinheiro"
+    },
+    {
+        id: "SE7",
+        expression: "Bicuda",
+        region: "Sudeste",
+        meaning: "Chutar a bola"
+    },
+    {
+        id: "SE8",
+        expression: "Cambito",
+        region: "Sudeste",
+        meaning: "Perna fina"
+    },
+    {
+        id: "SE9",
+        expression: "Frisete",
+        region: "Sudeste",
+        meaning: "Grampo para prender os cabelos"
+    },
+    {
+        id: "SE10",
+        expression: "Marombado",
+        region: "Sudeste",
+        meaning: "Pessoa viciada em musculação, musculoso"
+    },
+    {
+        id: "SE11",
+        expression: "Paga pau",
+        region: "Sudeste",
+        meaning: "Admirar algo ou alguém"
+    },
+    {
+        id: "SE12",
+        expression: "Rolê",
+        region: "Sudeste",
+        meaning: "Dar um passeio, volta"
+    },
+    {
+        id: "SE13",
+        expression: "Tá ligado?",
+        region: "Sudeste",
+        meaning: "Tá atento? Tá sabendo?"
+    },
+    {
+        id: "SE14",
+        expression: "Bater um rango",
+        region: "Sudeste",
+        meaning: "Matar a fome"
+    },
+    {
+        id: "SE15",
+        expression: "Bocada",
+        region: "Sudeste",
+        meaning: "Lugar perigoso, mal frequentado"
+    },
+    {
+        id: "SE16",
+        expression: "Buzão",
+        region: "Sudeste",
+        meaning: "Ônibus"
+    },
+    {
+        id: "SE17",
+        expression: "Brother",
+        region: "Sudeste",
+        meaning: "amigo"
+    },
+    {
+        id: "SE18",
+        expression: "Chapar o coco",
+        region: "Sudeste",
+        meaning: "Beber todas"
+    },
+    {
+        id: "SE19",
+        expression: "Mauricinho",
+        region: "Sudeste",
+        meaning: "Rico, moderninho, da elite"
+    },
+    {
+        id: "SE20",
+        expression: "Queimar o filme",
+        region: "Sudeste",
+        meaning: "Fazer fofoca"
+    },
+    {
+        id: "SE21",
+        expression: "Sangue bom",
+        region: "Sudeste",
+        meaning: "Pessoa legal, gente fina"
+    },
+    {
+        id: "SE22",
+        expression: "Só o pó",
+        region: "Sudeste",
+        meaning: "Cansado"
+    },
+    {
+        id: "SE23",
+        expression: "Treta",
+        region: "Sudeste",
+        meaning: "Confusão, briga"
+    },
+    {
+        id: "SE24",
+        expression: "Trocar ideia",
+        region: "Sudeste",
+        meaning: "Conversar"
+    },
+    {
+        id: "SE25",
+        expression: "Zueira ou zoação",
+        region: "Sudeste",
+        meaning: "Bagunça, brincadeira"
+    },
+]
+
+export default regionalExpressionsSE;
