44eae76821f50b2b08430e7caeba702e4604ebef
LuisBelo <luzaidinbelo@gmail.com>
2020-10-14 22:37:27 -0300

    refactor: add S expressions

---
+++ src/RegionalExpression/RegionalExpressionS.js
@@ -0,0 +1,82 @@
+const regionalExpressionsS = [
+    {
+        id: "S1",
+        expression: "Alçar a perna",
+        region: "Sul",
+        meaning: "Montar a cavalo"
+    },
+    {
+        id: "S2",
+        expression: "Campo santo",
+        region: "Sul",
+        meaning: "Cemitério"
+    },
+    {
+        id: "S3",
+        expression: "Embretar-se",
+        region: "Sul",
+        meaning: "Meter-se em apuros"
+    },
+    {
+        id: "S4",
+        expression: "Guacho",
+        region: "Sul",
+        meaning: "Animal ou pessoa criada sem mãe ou sem leite materno"
+    },
+    {
+        id: "S5",
+        expression: "Lindeiro",
+        region: "Sul",
+        meaning: "Ao lado, vizinho"
+    },
+    {
+        id: "S6",
+        expression: "Maleva",
+        region: "Sul",
+        meaning: "Bandido, malfeitor, perverso"
+    },
+    {
+        id: "S7",
+        expression: "Olada",
+        region: "Sul",
+        meaning: "Ocasião, oportunidade"
+    },
+    {
+        id: "S8",
+        expression: "Relho",
+        region: "Sul",
+        meaning: "Chicote pequeno com cabo de madeira e cabo torcido"
+    },
+    {
+        id: "S9",
+        expression: "Solito",
+        region: "Sul",
+        meaning: "Isolado, sozinho, sem companhia"
+    },
+    {
+        id: "S10",
+        expression: "Tirana",
+        region: "Sul",
+        meaning: "Cantiga e dança popular, variedade do fandango"
+    },
+    {
+        id: "S11",
+        expression: "Guri",
+        region: "Sul",
+        meaning: "Menino"
+    },
+    {
+        id: "S12",
+        expression: "Cacetinho",
+        region: "Sul",
+        meaning: "Pão francês"
+    },
+    {
+        id: "S13",
+        expression: "Desabotinado",
+        region: "Sul",
+        meaning: "Adoidado, estourado"
+    },
+]
+
+export default regionalExpressionsS;
