2034f6bad546282b3d098fccabc2d2abd1035f27
LuisBelo <luzaidinbelo@gmail.com>
2020-10-14 22:28:44 -0300

    refactor: add CO expressions

---
+++ src/RegionalExpression/RegionalExpressionCO.js
@@ -0,0 +1,118 @@
+const regionalExpressionsCO = [
+    {
+        id: "CO1",
+        expression: "Arruinou",
+        region: "Centro-Oeste",
+        meaning: "Piorou de saúde"
+    },
+    {
+        id: "CO2",
+        expression: "Carreta",
+        region: "Centro-Oeste",
+        meaning: "Carro"
+    },
+    {
+        id: "CO3",
+        expression: "Dormir no macio",
+        region: "Centro-Oeste",
+        meaning: "Viver folgado"
+    },
+    {
+        id: "CO4",
+        expression: "Empatar",
+        region: "Centro-Oeste",
+        meaning: "Atrapalhar"
+    },
+    {
+        id: "CO5",
+        expression: "Madurar",
+        region: "Centro-Oeste",
+        meaning: "Amadurecer"
+    },
+    {
+        id: "CO6",
+        expression: "Para de mula",
+        region: "Centro-Oeste",
+        meaning: "Perturbar"
+    },
+    {
+        id: "CO7",
+        expression: "Tá chovendo duro",
+        region: "Centro-Oeste",
+        meaning: "Chover torrencialmente"
+    },
+    {
+        id: "CO8",
+        expression: "Borracho",
+        region: "Centro-Oeste",
+        meaning: "Bêbado"
+    },
+    {
+        id: "CO9",
+        expression: "Não dar conta",
+        region: "Centro-Oeste",
+        meaning: "Não conseguir fazer alguma coisa"
+    },
+    {
+        id: "CO10",
+        expression: "Camela",
+        region: "Centro-Oeste",
+        meaning: "Bicicleta"
+    },
+    {
+        id: "CO11",
+        expression: "Azangar",
+        region: "Centro-Oeste",
+        meaning: "Estragar-se"
+    },
+    {
+        id: "CO12",
+        expression: "Abiscoitar",
+        region: "Centro-Oeste",
+        meaning: "Receber dinheiro, herdar-se"
+    },
+    {
+        id: "CO13",
+        expression: "Patife",
+        region: "Centro-Oeste",
+        meaning: "Mole, tímido, medroso"
+    },
+    {
+        id: "CO14",
+        expression: "Bitelo",
+        region: "Centro-Oeste",
+        meaning: "Grande; enorme"
+    },
+    {
+        id: "CO15",
+        expression: "Bugre",
+        region: "Centro-Oeste",
+        meaning: "Índio, tapuia, selvagem"
+    },
+    {
+        id: "CO16",
+        expression: "Carcunda, cacunda",
+        region: "Centro-Oeste",
+        meaning: "Costas, dorso do corpo humano"
+    },
+    {
+        id: "CO17",
+        expression: "Desguaritar",
+        region: "Centro-Oeste",
+        meaning: "Desgarrar, arribar, fugir"
+    },
+    {
+        id: "CO18",
+        expression: "Pé-de-boi",
+        region: "Centro-Oeste",
+        meaning: "Pessoa muito trabalhadora"
+    },
+    {
+        id: "CO19",
+        expression: "Perrengue",
+        region: "Centro-Oeste",
+        meaning: "Adoentado, convalescente, enfraquecido"
+    },
+]
+
+export default regionalExpressionsCO;
