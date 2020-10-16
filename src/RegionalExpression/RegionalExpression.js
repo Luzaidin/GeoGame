ac39dfca50666293ceab5b4860809529cdb4c1e7
LuisBelo <luzaidinbelo@gmail.com>
2020-10-15 22:14:43 -0300

    refactor: join all regional expressions

---
+++ src/RegionalExpression/RegionalExpression.js
@@ -0,0 +1,15 @@
+import regionalExpressionsN from './RegionalExpressionN';
+import regionalExpressionsNE from './RegionalExpressionNE';
+import regionalExpressionsCO from './RegionalExpressionCO';
+import regionalExpressionsS from './RegionalExpressionS';
+import regionalExpressionsSE from './RegionalExpressionSE';
+
+const regionalExpressions = [
+    ...regionalExpressionsN,
+    ...regionalExpressionsNE,
+    ...regionalExpressionsCO,
+    ...regionalExpressionsS,
+    ...regionalExpressionsSE
+] 
+
+export default regionalExpressions;
