9b272e1ca19c829d4d0904d354048296730ded46
LuisBelo <luzaidinbelo@gmail.com>
2020-10-15 21:40:02 -0300

    refactor: add NE expressions

---
+++ src/RegionalExpression/RegionalExpressionNE.js
@@ -0,0 +1,160 @@
+const regionalExpressionsNE = [
+    {
+        id: "NE1",
+        expression: "Abestado",
+        region: "Nordeste",
+        meaning: "Bobo, leso, tolo"
+    },
+    {
+        id: "NE2",
+        expression: "Abirobado",
+        region: "Nordeste",
+        meaning: "Maluco"
+    },
+    {
+        id: "NE3",
+        expression: "Abufelar",
+        region: "Nordeste",
+        meaning: "Irritar, ficar brabo"
+    },
+    {
+        id: "NE4",
+        expression: "Amancebado",
+        region: "Nordeste",
+        meaning: "Amigado, aquele que vive maritalmente com outra"
+    },
+    {
+        id: "NE5",
+        expression: "Amarrado",
+        region: "Nordeste",
+        meaning: "Mesquinho; avarento"
+    },
+    {
+        id: "NE6",
+        expression: "Arretado",
+        region: "Nordeste",
+        meaning: "Tudo que é bom; bacana; legal"
+    },
+    {
+        id: "NE7",
+        expression: "Avariado das ideias",
+        region: "Nordeste",
+        meaning: "Meio amalucado"
+    },
+    {
+        id: "NE8",
+        expression: "Avexado",
+        region: "Nordeste",
+        meaning: "Apressado"
+    },
+    {
+        id: "NE9",
+        expression: "Bater a caçoleta",
+        region: "Nordeste",
+        meaning: "Morrer"
+    },
+    {
+        id: "NE10",
+        expression: "Bizonho",
+        region: "Nordeste",
+        meaning: "Triste, calado"
+    },
+    {
+        id: "NE11",
+        expression: "Brenha",
+        region: "Nordeste",
+        meaning: "Lugar longe de difícil acesso; escuro"
+    },
+    {
+        id: "NE12",
+        expression: "Briba",
+        region: "Nordeste",
+        meaning: "Pequena lagartixa"
+    },
+    {
+        id: "NE13",
+        expression: "Bruguelo",
+        region: "Nordeste",
+        meaning: "Criança pequena"
+    },
+    {
+        id: "NE14",
+        expression: "Amofinado",
+        region: "Nordeste",
+        meaning: "Aborrecido"
+    },
+    {
+        id: "NE15",
+        expression: "Azuretado",
+        region: "Nordeste",
+        meaning: "Confuso"
+    },
+    {
+        id: "NE16",
+        expression: "Botar as barbas de molho",
+        region: "Nordeste",
+        meaning: "Tomar as devidas precauções"
+    },
+    {
+        id: "NE17",
+        expression: "Cafuringa",
+        region: "Nordeste",
+        meaning: "Coisa muito pequena"
+    },
+    {
+        id: "NE18",
+        expression: "Estrambólico",
+        region: "Nordeste",
+        meaning: "Esquisito, extravagante"
+    },
+    {
+        id: "NE19",
+        expression: "Fuzuê",
+        region: "Nordeste",
+        meaning: "Barulho, confusão"
+    },
+    {
+        id: "NE20",
+        expression: "Guenzo",
+        region: "Nordeste",
+        meaning: "Magro, esquelético"
+    },
+    {
+        id: "NE21",
+        expression: "Macambúzio",
+        region: "Nordeste",
+        meaning: "Tristonho, pensativo"
+    },
+    {
+        id: "NE22",
+        expression: "Rebole no mato",
+        region: "Nordeste",
+        meaning: "Jogue fora"
+    },
+    {
+        id: "NE23",
+        expression: "Sustança",
+        region: "Nordeste",
+        meaning: "Força, vigor"
+    },
+    {
+        id: "NE24",
+        expression: "Aperrear",
+        region: "Nordeste",
+        meaning: "Encher o saco"
+    },
+    {
+        id: "NE25",
+        expression: "Buchuda",
+        region: "Nordeste",
+        meaning: "Gestante"
+    },
+    {
+        id: "NE26",
+        expression: "Desmantelar",
+        region: "Nordeste",
+        meaning: "Arruinar"
+    },
+]
+
+export default regionalExpressionsNE;
